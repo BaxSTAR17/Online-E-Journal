5224f7e9e31f09959c5fb85e58d76c894e4536cb
BaxSTAR <baxford03@gmail.com>
2023-11-06 00:03:45 +0800

    added About

--- scripter.js
+++ scripter.js
@@ -13,21 +13,123 @@ let bchurch = document.querySelector(".bchurch"),
     lbox1 = document.querySelector(".lbox1"),
     lbox2 = document.querySelector(".lbox2"),
     bbox = document.querySelector(".btnbox"),
+    linkStyle = document.getElementById('linkstyle'),
     styler = document.head.appendChild(document.createElement("style"));
+    // bgMusic = new Audio("./assets/music/church.ogg");
 
-bchurch.addEventListener("click", function() {styler.innerHTML = ".bgchurch::before { opacity: 1; } .bgchurch::after { opacity: 1; } .p1 { transform: translate(40dvw, 0); } .p2 { transform: translate(-5dvw, 0); } .tbox1 { transform: translate(-2dvw, -58dvh); width: 550px; } .tbox2 { transform: translate(-3dvw, -57dvh); width: 200px; } .lbox1 { width: 200px; transform: translate(0, -23dvh); } .lbox2 { width: 200px; transform: translate(-15dvw, 6dvh); } .gochurch { visibility: visible; opacity: 1;}"; bbox.textContent = "GO BACK";});
-bclassroom.addEventListener("click", function() {styler.innerHTML = ".bgclassroom::before { opacity: 1; } .bgclassroom::after { opacity: 1; } .p1 { transform: translate(35dvw, 54dvh); } .p2 { transform: translate(-5dvw, -52dvh); } .tbox1 { transform: translate(8dvw, -1dvh); } .tbox2 { width: 600px; transform: translate(-42dvw, -53dvh); } .lbox1 { width: 210px; transform: translate(15dvw, -30dvh); } .lbox2 { width: 210px; transform: translate(-1dvw, -1dvh);} .goclassroom { visibility: visible; opacity: 1;}"; bbox.textContent = "GO BACK"});
-bcommunity.addEventListener("click", function() {styler.innerHTML = ".bgcommunity::before { opacity: 1; } .bgcommunity::after { opacity: 1; } .p1 { transform: translate(-40dvw, 0); } .p2 { transform: translate(-13dvw, 0);} .tbox1 { transform: translate(-12dvw, -1dvh); width: 400px; } .tbox2 { transform: translate(-45dvw, -53dvh); width: 400px; } .lbox1 { width: 150px; transform: translate(-20dvw, -88dvh); } .lbox2 { width: 150px; transform: translate(-20dvw, -5dvh); } .gocommunity { visibility: visible; opacity: 1;}"; bbox.textContent = "GO BACK"});
-bculture.addEventListener("click", function() {styler.innerHTML = ".bgculture::before { opacity: 1; } .bgculture::after { opacity: 1; } .p1 { transform: translate(29dvw, 54dvh); } .tbox1 { width: 650px; transform: translate(0, -60dvh); } .tbox2 { width: 300px; transform: translate(4dvw, -57dvh);} .lbox1 { width: 300px; transform: translate(-35dvw, -80dvh); } .lbox2 { width: 300px; transform: translate(-35dvw, -40dvh);} .goculture { visibility: visible; opacity: 1;}"; bbox.textContent = "GO BACK"});
-bcourt.addEventListener("click", function() {styler.innerHTML = ".bgcourt::before { opacity: 1; } .bgcourt::after { opacity: 1; } .p2 { transform: translate(27dvw, 0); } .tbox1 { width: 400px; transform: translate(-30dvw, -3dvh); } .tbox2 { width: 400px; transform: translate(-63dvw, -55dvh); } .lbox1 { width: 300px; transform: translate(20dvw, -23dvh); } .lbox2 { width: 600px; transform: translate(29dvw, 10dvh);} .gocourt { visibility: visible; opacity: 1;}"; bbox.textContent = "GO BACK"});
+// bgMusic.addEventListener("ended", repeatBg);
+// bgMusic.volume = 0.1;
+
+// function repeatBg(e) {
+//     e.target.currentTime = 0;
+//     e.target.play();
+// }
+
+// function playBg(n) {
+//     bgMusic.pause();
+    // switch(n) {
+    //     case 1:
+    //         bgMusic = new Audio("./assets/music/church.ogg");
+    //         break;
+    //     case 2:
+    //         bgMusic = new Audio("./assets/music/classroom.ogg");
+    //         break;
+    //     case 3:
+    //         bgMusic = new Audio("./assets/music/culture.ogg");
+    //         break;
+    //     case 4:
+    //         bgMusic = new Audio("./assets/music/community.ogg");
+    //         break;
+    //     case 5:
+    //         bgMusic = new Audio("./assets/music/court.ogg");
+    //         break;
+    // }
+//     bgMusic.play();
+//     bgMusic.volume = 0.08;
+// }
+
+bchurch.addEventListener("click", function() {
+    if (bbox.textContent == "GO☨BACK") {
+        bboxfunc() //go back function
+    }
+    else {
+        linkStyle.href = "./stylers/church.css"; 
+        bbox.textContent = "GO☨BACK";
+        lbox1.textContent = "Points in total: ";
+        lbox2.textContent = "Entries in total: ";
+        lbox1.style.fontWeight = "1";
+        playBg(1);
+    }
+});
+
+bclassroom.addEventListener("click", function() {
+    if (bbox.textContent == "GO✓BACK") {
+        bboxfunc()
+    }
+    else {
+        linkStyle.href = "./stylers/classroom.css"; 
+        bbox.textContent = "GO✓BACK";
+        lbox1.textContent = "Total Points: ";
+        lbox2.textContent = "Total Entries: ";
+        lbox1.style.fontWeight = "1";
+        playBg(2);
+    }
+});
+
+bcommunity.addEventListener("click", function() {
+    if (bbox.textContent == "GO⚧BACK") {
+        bboxfunc()
+    }
+    else {
+        linkStyle.href = "./stylers/community.css"; 
+        bbox.textContent = "GO⚧BACK";
+        lbox1.textContent = "About Community:";
+        lbox2.textContent = "Goal and Aim:";
+        lbox1.style.fontWeight = "1";
+        playBg(4);
+    }
+});
+
+bculture.addEventListener("click", function() {
+    if (bbox.textContent == "GO⍟BACK") {
+        bboxfunc()
+    }
+    else {
+        linkStyle.href = "./stylers/culture.css"; 
+        bbox.textContent = "GO⍟BACK";
+        lbox1.textContent = "Entry Count: ";
+        lbox2.textContent = "Point Tally: ";
+        lbox1.style.fontWeight = "1";
+        playBg(3);
+    }
+});
+
+bcourt.addEventListener("click", function() {
+    if (bbox.textContent == "GO◍BACK") {
+        bboxfunc()
+    }
+    else {
+        linkStyle.href = "./stylers/court.css"; 
+        bbox.textContent = "GO◍BACK";
+        lbox1.textContent = "EXAMPLES OF SPORTS IN SILLIMAN ARE: ";
+        lbox1.style.fontWeight = "550";
+        lbox2.textContent = "Basketball, Badminton, Chess, Volleyball, Swimming, Softball, Football, E-Sports, etc. ";
+        playBg(5);
+    }
+});
+
 bbox.addEventListener("click", bboxfunc);
 
-function bboxfunc(b) {
-    if (bbox.textContent == "GO BACK") {
-        styler.innerHTML = ".bgchurch::before { opacity: 0; } .bgclassroom::before { opacity: 0; } .bgcommunity::before { opacity: 0; } .bgculture::before { opacity: 0; } .bgcourt::before { opacity: 0; }"; 
-        bbox.textContent = "ABOUT";
-    }
-    else if (bbox.textContent == "ABOUT") {
+function bboxfunc() {
+    if (bbox.textContent == "ABOUT PAGE") {
         window.location.href = "./about/index.htm";
+    }
+    else {
+        linkStyle.href = "./stylers/goback.css";
+        bbox.textContent = "ABOUT PAGE";
+        lbox1.textContent = "Made by: Baxter Bao-As GE10A-O © 2023";
+        lbox2.textContent = "Welcome to my WPE Online Journal! where fun comes alive!";
+        lbox1.style.fontWeight = "1";
+        bgMusic.pause();
     }
 }

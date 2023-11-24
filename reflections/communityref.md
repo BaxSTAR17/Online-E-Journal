2c191bc95bf93abe93329e54cb8d65d8e27f9d68
BaxSTAR <baxford03@gmail.com>
2023-11-24 23:47:05 +0800

    Homepage mostly complete

--- reflections/communityref.md
+++ reflections/communityref.md
@@ -17,10 +17,22 @@ This community event influenced me to express myself more publicly whether it be
 Because of this event, I feel a lot happy knowing that there are a lot of people that are creative but are also willing to stand up for what they think is right.
 
 <!-- #               SIGN ME UP: DEAF CULTURE AND A WAY OF LIFE>
+The Sign Me Up: Deaf Culture and a Way of life is a community engagement event that reaches out to the deaf community and also promotes and commemorates international sign language day, a holiday celebrating one of the most common mediums of communication for deaf or mute people. The event was held on October 14, 2023, 10:00 AM at the PEP Room of the Oriental Hall. The event was open to all students of different colleges so that a lot of people would experience what its like communicating and cooperating with the deaf community.
 
+The event went over my expectations and I enjoyed it more than I expected because it was filled with lots of interactive and fun games and even sharing that helped us connect with our deaf guests. Everyone was really enjoying their time especially our deaf guests that facilitated these games themselves. One of the best parts of the activity is the last portion of the event where we get to ask questions personally to one of our guests like an open forum about sign language and the deaf community while a translator would translate our questions.
+
+The activity taught us some useful sign language actions and keywords that would help us communicate in the smallest ways to other deaf people and can encourage us to even learn the whole vocabulary of sign language. With these learnings, I can apply this in my future whenever I encounter a deaf or mute person so that there won't be any communication barriers that would hinder us and that's a big benefit.
+
+Because of this event, I am more knowledgable of sign language and aware of the detrimental situations and challenges deaf people have to face to just communicate.
 
 <!-- #               LOCAL FARMERS' TABO>
+The Local Farmers' Tabo is an agricultural fair event that promotes and helps our local farmers to sell their products to recognize and pay them for their hardwork. The event occurred on October 28, 2023, 8:00 AM at Guy Hall. The event was open to the entire university and everyone was free to buy whatever from our local farmers. As a student of Silliman University, I was more than willing to go to this event and buy what they have to offer because our local products should be recognized.
 
+The event was successful and the local farmers were able to have their products sold well. The prices were also very cheap and negotiable, it was a perfect agricultural fair event. One of the stalls even sell a lot of my favorite fruits that I miss eating back in my hometown. The sellers ere also very friendly and welcoming and they really serve as the highlight of this event.
+
+This event alone can teach us and influence us to explore and unravel more of our local products other than just dwelling with foreign products. It can give us the insight that these farmers are trying to make a living out of their hardwork and is trying to gain the best outcome from their hardwork and being able to buy from them feels like I've helped them in many ways more than one.
+
+Because of this event, I am further determined to give more attention to local products and even promote these to other people especially of my own country.
 
 <!-- #               VIGIL THEATRICAL PLAY>
 
--- scripter.js
+++ scripter.js
@@ -66,9 +66,12 @@ bchurch.addEventListener("click", function() {
         linkStyle.href = "./stylers/church.css"; 
         bchurch.classList.add("selected");
         bbox.textContent = "GO☨BACK";
-        lbox1.textContent = "Points in total: ";
-        lbox2.textContent = "Entries in total: ";
+        lbox1.textContent = "Points in total: 9 points";
+        lbox2.textContent = "Entries in total: 8 entries";
         lbox1.style.fontWeight = "1";
+        tbox1.innerHTML = "&nbsp&nbsp&nbsp&nbspThe first C is Church! This records all religious events I've attended within the semester that helped shape my spiritual well-being, faith, and devotion to God. The activities included in this portion can range from religious masses, fellowship services, worship services, devotions, and other more eucharistic celebrations. In order to see my events, press the 'GO' button displayed on the left!"
+        tbox2.style.fontSize = "13.5px";
+        tbox2.innerHTML = "FUN FACTS ABOUT SU CHURCH: <br /> <ul> <li> It is the tallest building of the university </li> <li> It is a protestant church </li> <li> Its view of the sunrise is not obstructed by anything </li> </ul>"
         playBg(1);
     }
 });
@@ -80,9 +83,12 @@ bclassroom.addEventListener("click", function() {
     else {
         linkStyle.href = "./stylers/classroom.css"; 
         bbox.textContent = "GO✓BACK";
-        lbox1.textContent = "Total Points: ";
-        lbox2.textContent = "Total Entries: ";
+        lbox1.textContent = "Total Points: 9";
+        lbox2.textContent = "Total Entries: 8";
         lbox1.style.fontWeight = "1";
+        tbox1.innerHTML = "&nbsp&nbsp&nbsp&nbspThe second C is classroom! This promotes education and learning as it is the central core of any school in the world. Events under classroom enhances and improves our knowledge and mental prowess so that we could put it to good use in the future! In order to view my classroom events, press the 'GO' button on the left!"
+        tbox2.innerHTML = "The Activities Under Classroom can be as follows: <br /> <ul> <li> Lectures, and sypmosiums </li> <li> orientations and discussions </li> <li> workshops and talks </li> <li> classroom activities and educational activities </li> </ul>"
+        tbox2.style.fontSize = "21px";
         playBg(2);
     }
 });
@@ -95,8 +101,11 @@ bcommunity.addEventListener("click", function() {
         linkStyle.href = "./stylers/community.css"; 
         bbox.textContent = "GO⚧BACK";
         lbox1.textContent = "About Community:";
-        lbox2.textContent = "Goal and Aim:";
+        lbox2.textContent = "Activities under Community:";
         lbox1.style.fontWeight = "1";
+        tbox1.innerHTML = "&nbsp&nbsp&nbsp&nbspActivities and events under community can include anything related to community service. This includes fundraiser events that collects funds for a greater cause. Outreaches that gives you the chance to directly connect and serve a certain community. Even donations and white gift distributions can be included under community."
+        tbox2.innerHTML = "&nbsp&nbsp&nbsp&nbspThe fourth C is community! It covers community service events that promote good advocacies and practice beneficial events that helps out different communities and allows us to connect to them more and be with them. There are a total of 8 entries and 8 points total for this journal portion. If you want to see the events for community, Press the 'GO' button displayed at the bottom right!"
+        tbox2.style.fontSize = "16px";
         playBg(4);
     }
 });
@@ -108,9 +117,12 @@ bculture.addEventListener("click", function() {
     else {
         linkStyle.href = "./stylers/culture.css"; 
         bbox.textContent = "GO⍟BACK";
-        lbox1.textContent = "Entry Count: ";
-        lbox2.textContent = "Point Tally: ";
+        lbox1.textContent = "Entry Count: 8 ENTRIES";
+        lbox2.textContent = "Point Tally: 12 PTS.";
         lbox1.style.fontWeight = "1";
+        tbox1.innerHTML = "&nbsp&nbsp&nbsp&nbspThe third C is culture! The C with one of tne of the most colorful, intriguing, fun and unique events that are part of the culture of the place that can also promote culture of anything whether it may be of other countries or even organizations and communities. If you want to look at the events, navigate to the 'GO' button displayed at the bottom!"
+        tbox2.innerHTML = "The Events inside Culture can be as follows: <br /> <ul> <li> Festivals and Fiestas </li> <li> Parades and Walks </li> <li> Traditional events </li> <li> International events </li> </ul>"
+        tbox2.style.fontSize = "20px";
         playBg(3);
     }
 });
@@ -125,6 +137,9 @@ bcourt.addEventListener("click", function() {
         lbox1.textContent = "EXAMPLES OF SPORTS IN SILLIMAN ARE: ";
         lbox1.style.fontWeight = "550";
         lbox2.textContent = "Basketball, Badminton, Chess, Volleyball, Swimming, Softball, Football, E-Sports, etc. ";
+        tbox1.innerHTML = "&nbsp&nbsp&nbsp&nbspVENUES IN SILLIMAN UNIVERSITY FOR SPORT EVENTS: <br /> <ul> <li> RAVELLO BALLFIELD </li> <li> CIMAFRANCA BALLFIELD </li> <li> ALASKA COURT </li> <li> LAWN TENNIS COURT </li> <li> SHAW MEMORIAL POOL </li> <li> MONAPIL COURT </li> </ul>"
+        tbox2.innerHTML = "&nbsp&nbsp&nbsp&nbspThe fiftth C is Court! It refers to the usual venue and place of most sports and physical competitions that promotes sportsmanship, excellenece, skill, teamwork, physical ability and strategy all in one! The Court portion of my journal has a total of 7 entries and 8s points. If you want my different sports events to be shown, press the 'GO' button on the right!"
+        tbox2.style.fontSize = "18px";
         playBg(5);
     }
 });
@@ -132,15 +147,33 @@ bcourt.addEventListener("click", function() {
 bbox.addEventListener("click", bboxfunc);
 
 function bboxfunc() {
-    if (bbox.textContent == "ABOUT PAGE") {
+    if (bbox.textContent == "ABOUT ME") {
         window.location.href = "./about/index.htm";
     }
     else {
         linkStyle.href = "./stylers/goback.css";
-        bbox.textContent = "ABOUT PAGE";
+        bbox.textContent = "ABOUT ME";
         lbox1.textContent = "Made by: Baxter Bao-As GE10A-O © 2023";
         lbox2.textContent = "Welcome to my WPE Online Journal! where fun comes alive!";
+        tbox1.innerHTML = "&nbsp&nbsp&nbsp&nbspIn this Online Journal, you'll be able to discover and view events that I've participated in and experienced that helped shape me as a person. This helps me live and practice the 5 Cs of Silliman University. In order to see these events per C, navigate through the buttons in the button widget above. If you wanna learn more about me, See 'About Me', Hope you enjoy my online journal! Let the fun commence!";
+        tbox2.innerHTML = "";
         lbox1.style.fontWeight = "1";
         bgMusic.pause();
     }
+}
+
+//TODO rotate widgetholder 
+document.addEventListener("mousemove", function(e){
+    let x = e.clientX;
+    let y = e.clientY;
+    const middleX = window.innerWidth / 2;
+    const middleY = window.innerHeight / 2;
+    let offsetX = ((x - middleX) / middleX) * 45;
+    let offsetY = ((y - middleY) / middleY) * 45;
+    roble(offsetX, offsetY, btnwrapper);
+});
+
+function roble(x, y, element) {
+    element.style.setProperty("--rotX", -1 * y + "deg");
+    element.style.setProperty("--rotY", x + "deg");
 }
